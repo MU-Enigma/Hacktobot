d67954c239f9dc74b334f5a1538ba70c968e5889
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-10-02 19:38:19 +0530

    Flask server for webhooks

--- hacktobot.py
+++ hacktobot.py
@@ -1,13 +1,49 @@
-from github import Github
+"""
+	Server to listen to pull requests using GitHub webhooks.
+	
+	Outer shell to co-ordinate between all other actions
+"""
 
-g = Github("key_here")
+from flask import Flask, request, abort
+from json import dump
+import hmac
+import hashlib
+import os
 
-repo = g.get_repo("MU-Enigma/Hacktoberfest_2020")
+app = Flask(__name__)
 
-# print(dir(repo))
-for commit in repo.get_commits():
-	try:
-		print(commit.committer.login)
-	except Exception as e:
-		print(f"Something went wrong bro: {e}")
-		print(commit.raw_data["commit"]["committer"]["name"])
+GITHUB_SECRET = os.environ['GITHUB_SECRET']
+
+@app.route("/")
+def home():
+	return "Webhook is up and running!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	if request.method == 'POST':
+
+		# Extract signature header
+		signature = request.headers.get("X-Hub-Signature")
+		if not signature or not signature.startswith("sha1="):
+			abort(400, "X-Hub-Signature required")
+
+		# Create local hash of payload
+		digest = hmac.new(GITHUB_SECRET.encode(), request.data, hashlib.sha1).hexdigest()
+
+		# Verify signature
+		if not hmac.compare_digest(signature, "sha1=" + digest):
+			abort(400, "Invalid signature")
+
+		if(request.json['action'] == 'open'):
+			pass
+
+			# request.json['number'] gives the PR number
+
+			# TODO: Check if the PR is valid
+			# TODO: Check for the files
+			# TODO: Execute and send the output on discord
+		
+
+		return 'success', 200
+	else:
+		abort(400)
